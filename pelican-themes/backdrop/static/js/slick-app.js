73ebe3e5c97cb7422e0505ad306b575f321ac08d
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2018-04-16 12:09:53 +0200

    Wider image-pane, slower carousel, move pages to right, add class site-description, add emblems

--- pelican-themes/backdrop/static/js/slick-app.js
+++ pelican-themes/backdrop/static/js/slick-app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel').slick({
       dots: true,
-      //autoplay: true,
-      //autoplaySpeed: 2000,
+      autoplay: true,
+      autoplaySpeed: 5000,
       slidesToShow: 1,
       slidesToScroll: 1,
 
